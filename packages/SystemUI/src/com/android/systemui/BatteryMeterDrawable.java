e0e56641292e14c80a768e4f513162e1e6479a89
DennySPB <dennyspb@gmail.com>
2017-06-30 15:06:00 +0300

    Pulsing battery icon

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -282,8 +282,12 @@ public class BatteryMeterDrawable extends Drawable implements
         }
 
 	
-	if (mStyle == BATTERY_STYLE_SOLID) {
-            animateSolidBattery(level, pluggedIn, charging);
+//	if (mStyle == BATTERY_STYLE_SOLID) {
+//            animateSolidBattery(level, pluggedIn, charging);
+	if (Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.STATUS_BAR_PULSE_CHARGING_BATTERY, 0) == 1 ||
+                mStyle == BATTERY_STYLE_SOLID) {
+            pulseBatteryIcon(level, pluggedIn, charging);
 	}
         postInvalidate();
     }
@@ -394,7 +398,7 @@ public class BatteryMeterDrawable extends Drawable implements
         }
     }
 
-    public void animateSolidBattery(int level, boolean pluggedIn, boolean charging) {
+    public void pulseBatteryIcon(int level, boolean pluggedIn, boolean charging) {
         if (charging) {
             if (mAnimator != null) mAnimator.cancel();
 
